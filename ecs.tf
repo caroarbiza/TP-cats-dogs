f93ff87b80bd96d9070e85ec88291b08d47844d5
Caro <CA252755@fi365.ort.edu.uy>
2021-11-17 17:15:46 -0300

    se optimizo el codigo, las dos app corriendo con lb

--- ecs.tf
+++ ecs.tf
@@ -34,31 +34,6 @@ resource "aws_ecs_task_definition" "taskcat1" {
   ])
 }
 
-resource "aws_ecs_task_definition" "taskcat2" {
-  family                   = "taskcat"
-  requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = "arn:aws:iam::813627017350:role/AmazonECSTaskExecutionRolePolicy"
-  network_mode             = "awsvpc"
-  cpu                      = 256
-  memory                   = 512
-  container_definitions = jsonencode([
-    {
-      name  = "second-cat"
-      image = "813627017350.dkr.ecr.us-east-1.amazonaws.com/cats"
-
-      essential = true
-
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-        }
-      ]
-    }
-
-  ])
-}
-
 resource "aws_ecs_task_definition" "taskdog1" {
   family                   = "taskdog"
   requires_compatibilities = ["FARGATE"]
@@ -84,36 +59,13 @@ resource "aws_ecs_task_definition" "taskdog1" {
   ])
 }
 
-resource "aws_ecs_task_definition" "taskdog2" {
-  family                   = "taskdog"
-  requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = "arn:aws:iam::813627017350:role/AmazonECSTaskExecutionRolePolicy"
-  network_mode             = "awsvpc"
-  cpu                      = 256
-  memory                   = 512
-  container_definitions = jsonencode([
-    {
-      name  = "second-dog"
-      image = "813627017350.dkr.ecr.us-east-1.amazonaws.com/dogs"
-
-      essential = true
-
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-        }
-      ]
-    }
-
-  ])
-}
 
 resource "aws_ecs_service" "ecs_taskcat1" {
   name            = "ecs_taskcat1"
   task_definition = aws_ecs_task_definition.taskcat1.arn
   cluster         = aws_ecs_cluster.cluster_catdog.id
   launch_type     = "FARGATE"
+  desired_count   = 2
   network_configuration {
     subnets          = [aws_subnet.subnet_public_one.id, aws_subnet.subnet_public_two.id]
     security_groups  = [aws_security_group.allow_http.id]
@@ -127,23 +79,7 @@ resource "aws_ecs_service" "ecs_taskcat1" {
   }
 }
 
-resource "aws_ecs_service" "ecs_taskcat2" {
-  name            = "ecs_taskcat2"
-  task_definition = aws_ecs_task_definition.taskcat2.arn
-  cluster         = aws_ecs_cluster.cluster_catdog.id
-  launch_type     = "FARGATE"
-  network_configuration {
-    subnets          = [aws_subnet.subnet_public_one.id, aws_subnet.subnet_public_two.id]
-    security_groups  = [aws_security_group.allow_http.id]
-    assign_public_ip = true
-  }
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.cat_tg.arn
-    container_name   = "second-cat"
-    container_port   = 80
-  }
-}
 
 
 resource "aws_ecs_service" "ecs_taskdog1" {
@@ -151,6 +87,7 @@ resource "aws_ecs_service" "ecs_taskdog1" {
   task_definition = aws_ecs_task_definition.taskdog1.arn
   cluster         = aws_ecs_cluster.cluster_catdog.id
   launch_type     = "FARGATE"
+  desired_count   = 2
   network_configuration {
     subnets          = [aws_subnet.subnet_public_one.id, aws_subnet.subnet_public_two.id]
     security_groups  = [aws_security_group.allow_http.id]
@@ -164,22 +101,6 @@ resource "aws_ecs_service" "ecs_taskdog1" {
   }
 }
 
-resource "aws_ecs_service" "ecs_taskdog2" {
-  name            = "ecs_taskdog2"
-  task_definition = aws_ecs_task_definition.taskdog2.arn
-  cluster         = aws_ecs_cluster.cluster_catdog.id
-  launch_type     = "FARGATE"
-  network_configuration {
-    subnets          = [aws_subnet.subnet_public_one.id, aws_subnet.subnet_public_two.id]
-    security_groups  = [aws_security_group.allow_http.id]
-    assign_public_ip = true
-  }
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.dog_tg.arn
-    container_name   = "second-dog"
-    container_port   = 80
-  }
-}
 
 
